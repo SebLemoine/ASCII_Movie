9fc3f4e9bec98dc0ae761509c009811c73c8ecf4
Sébastien LEMOINE <lemoine.sebastien15@gmail.com>
2017-02-03 21:06:02 +0100

    ADD 1rst version : début de quelques TODO sur Film.java

--- src/film/Film.java
+++ src/film/Film.java
@@ -9,32 +9,69 @@ public class Film {
 	private String createur;
 	private int tailleXPage;
 	private int tailleYPage;
-	
 	private int numeroDePageCourante;
 	
+	public Film(String titre,String createur,int tailleXPage, int tailleYPage){
+		this.titre = titre;
+		this.createur = createur;
+		this.tailleXPage = tailleXPage;
+		this.tailleYPage = tailleYPage;
+		this.test.add(new Page());
+		this.numeroDePageCourante = 0;
+	}
 	public void nouvellePage(){
 		/** TODO:nouvellePage */
+		this.test.add(new Page());
+		this.changerDePage(this.test.size()-1);
 	}
-	public void nouvellePageCopie(int numeroPage) {
+	public void nouvellePageCopie(int numeroPage ) {
 		/** TODO:nouvellePageCopie */
+		--numeroPage;
+		Page p;
+		if(this.estDansLeTableau(numeroPage)){
+			p = this.test.get(numeroPage);
+			this.test.add(p);
+		}
+		else System.err.println("error : choisir un nombre entre 1 et" + this.test.size());
+			
 	}
 	public void changerDePage(int numeroPage) {
 		/** TODO:changerDePage */
+		--numeroPage;
+		if(this.estDansLeTableau(numeroPage))
+			this.numeroDePageCourante = numeroPage;
 	}
 	public void pageSuperieur() {
 		/** TODO:pageSuperieur */
+		this.numeroDePageCourante++;
+		if(!this.estDansLeTableau(this.numeroDePageCourante)) numeroDePageCourante--;
 	}
 	public void pageInferieur() {
 		/** TODO:pageInferieur */
+		this.numeroDePageCourante--;
+		if(!this.estDansLeTableau(this.numeroDePageCourante)) numeroDePageCourante++;
 	}
 	public void supprimerPageCourante() {
 		/** TODO:supprimerPageCourante */
+		this.test.remove(numeroDePageCourante);
+		if(!this.estDansLeTableau(this.numeroDePageCourante)) numeroDePageCourante=0;
+		this.changerDePage(numeroDePageCourante);
+		
 	}
 	public void supprimerPage(int numeroPage) {
 		/** TODO:supprimerPage */
+		numeroPage--;
+		if(numeroPage == this.numeroDePageCourante){
+			supprimerPageCourante();
+		}else{
+			this.test.remove(this.numeroDePageCourante);
+			this.numeroDePageCourante=0;
+		}
+		
 	}
 	public void changePositionPageCourante(int nouvellePosition) {
 		/** TODO:changePositionPageCourante */
+		this.numeroDePageCourante = nouvellePosition;
 	}
 	public void changePosition(int numeroPage, int nouvellePosition ) {
 		/** TODO:changePosition */
@@ -48,7 +85,12 @@ public class Film {
 	public void play() {
 		/** TODO:play */
 	}
-
+	private boolean estDansLeTableau(int numero){
+		if( 0 <= numero && numero < this.test.size())
+			return true;
+		else
+			return false;
+	}
 	
 	
 }
